a515ee9ea7d00ae88d3592637103ddb686efb76b
ivo <ivan.ap9117@gmail.com>
2020-06-08 21:31:17 -0300

    se crea el packete dominio en el module-simple

--- module-simple/src/main/java/domainapp/modules/simple/dominio/operario/OperarioMenu.java
+++ module-simple/src/main/java/domainapp/modules/simple/dominio/operario/OperarioMenu.java
@@ -2,5 +2,7 @@ package domainapp.modules.simple.dominio.operario;
 
 public class OperarioMenu {
     public Operario create(String nombreyApellido, String legajoSAP, String empresa, String email, String telefono, String numeroLicencia, String vencimientoLicencia, Boolean llaveRSV, String clave, String confirmacion) {
+
     }
+
 }
