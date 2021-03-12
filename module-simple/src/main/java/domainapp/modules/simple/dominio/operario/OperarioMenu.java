8e4ba05cf4d76b15ac0331acfa409310df0189ec
ivo <ivan.ap9117@gmail.com>
2021-03-12 12:38:31 -0300

    Issue #7: Revision y correccion de codigo Se modifica visualizacion de nombres

--- module-simple/src/main/java/domainapp/modules/simple/dominio/operario/OperarioMenu.java
+++ module-simple/src/main/java/domainapp/modules/simple/dominio/operario/OperarioMenu.java
@@ -43,8 +43,8 @@ public class OperarioMenu {
             final String nombreyApellido,
 
             @Parameter(maxLength = 40,
-                    regexPattern = "^[0-9]{9}$",
-                    regexPatternReplacement = "Unicamente carga de números (9 como maximo)")
+                    regexPattern = "^[0-6]{6}$",
+                    regexPatternReplacement = "Unicamente carga de números (6 como maximo)")
             @ParameterLayout(named = "Legajo SAP: ")
             final String legajoSAP,
 
@@ -86,7 +86,7 @@ public class OperarioMenu {
 
         public Operario findByLegajoSAP(
                 @Parameter(optionality = Optionality.MANDATORY)
-                @ParameterLayout(named = "Buscar por legajoSAP: ")
+                @ParameterLayout(named = "Legajo SAP: ")
                 final String legajoSAP){
             TypesafeQuery<Operario> q = isisJdoSupport.newTypesafeQuery(Operario.class);
             final QOperario cand = QOperario.candidate();
